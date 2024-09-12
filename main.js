18eea3094ef59a1bdc557d62b9b139cc7b19d84a
mohammed-dhabaab <mohammed.dhabaab@gmail.com>
2024-09-12 10:56:04 +0300

    Completed

---
+++ main.js
@@ -0,0 +1,8 @@
+const btn1 = document.querySelector(".btn1")
+const btn2 = document.querySelector(".btn2")
+const btn3 = document.querySelector(".btn3")
+
+btn1.classList.add("hoverable")
+btn2.classList.add("hoverable")
+btn3.classList.add("hoverable")
+
